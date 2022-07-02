27dd95e3054d6e7685bd3def8cf2f59d3d0aef81
mcabanlit <102983286+mcabanlit@users.noreply.github.com>
2022-07-03 04:04:02 +0800

    API Initial Commit

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("Jello")
+import flask
+from flask import request, jsonify
+# from flask import Flask, send_from_directory, jsonify
+
+import pickle
+import numpy as np
+import pandas as pd
+
+model = pickle.load(open('sales_prediction.pkl', 'rb'))
+app = flask.Flask(__name__)
+
+@app.route("/predict", methods=['GET'])
+def predict():
+    arg = request.args['arg']
+    return jsonify(data="Hello from wtf.")
