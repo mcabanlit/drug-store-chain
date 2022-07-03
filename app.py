2d209d96e950e16b85440140090cf8a919908e5f
mcabanlit <102983286+mcabanlit@users.noreply.github.com>
2022-07-03 16:53:39 +0800

    Updated app.py to remove Pywebio

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ validation_empty = pd.read_csv("data/validation_empty.csv")
 model = pickle.load(open('sales_prediction_small.pkl', 'rb'))
 
 
-@app.route('/faq')
+@app.route('/')
 def index():
     """
     Index of our app, which basically just says to access the API via /predict
@@ -158,112 +158,28 @@ def predict_sales(input):
     # Return the forecasted value.
     return jsonify(sales=forecasted_sales)
 
-
-def welcome():
-    """
-    Sales Forecast
-    Predicts the sales given the required fields from user input.
-   """
-    put_text("")
-    welcome = input_group('What would you like to do?', [
-        actions('This web app uses Random Forest classifier on sales data in order '
-                'to try and predict sales. Please choose one of the options below to proceed.', [
-                    {'label': 'Predict Sales', 'value': 'make_prediction', 'color': 'info'},
-                    {'label': 'View Documentation', 'value': 'view_dataset', 'color': 'secondary'},
-                    {'label': 'Browse Code', 'value': 'browse_code', 'color': 'dark'},
-                ], name='action',
-                help_text='The links above are only granted to key personnel, if you would like access, please '
-                          'feel free to email mcabanlitph@gmail.com'),
-    ])
-
-
-app.add_url_rule('/', 'webio_view', webio_view(welcome),
-                 methods=['GET', 'POST', 'OPTIONS'])
+#
+# def welcome():
+#     """
+#     Sales Forecast
+#     Predicts the sales given the required fields from user input.
+#    """
+#     put_text("")
+#     welcome = input_group('What would you like to do?', [
+#         actions('This web app uses Random Forest classifier on sales data in order '
+#                 'to try and predict sales. Please choose one of the options below to proceed.', [
+#                     {'label': 'Predict Sales', 'value': 'make_prediction', 'color': 'info'},
+#                     {'label': 'View Documentation', 'value': 'view_dataset', 'color': 'secondary'},
+#                     {'label': 'Browse Code', 'value': 'browse_code', 'color': 'dark'},
+#                 ], name='action',
+#                 help_text='The links above are only granted to key personnel, if you would like access, please '
+#                           'feel free to email mcabanlitph@gmail.com'),
+#     ])
+#
+#
+# app.add_url_rule('/', 'webio_view', webio_view(welcome),
+#                  methods=['GET', 'POST', 'OPTIONS'])
 
 
 if __name__ == '__main__':
     app.run()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# from pywebio.platform.flask import webio_view
-# from pywebio import STATIC_PATH
-# from flask import Flask, send_from_directory, jsonify
-# from pywebio.input import *
-# from pywebio.output import *
-# import argparse
-# from pywebio import start_server
-# import pywebio
-# import pickle
-# import numpy as np
-# from pywebio import STATIC_PATH
-# import pandas as pd
-#
-# model = pickle.load(open('sales_prediction.pkl', 'rb'))
-# app = Flask(__name__)
-#
-# # @app.route("/")
-# def predict():
-#     welcome = input_group('What would you like to do?', [
-#         actions('This web app uses Random Forest classifier on more than a thousand datasets on heart-disease in order '
-#                 'to try and predict if a user has heart disease. Please choose one of the options below to proceed.', [
-#                     {'label': 'Check for Heart Disease', 'value': 'make_prediction', 'color': 'info'},
-#                     {'label': 'View Dataset', 'value': 'view_dataset', 'color': 'secondary'},
-#                     {'label': 'Browse Code', 'value': 'browse_code', 'color': 'dark'},
-#                 ], name='action',
-#                 help_text='This model uses factors such as age, sex, chest pain, blood pressure, serum '
-#                           'cholesterol, fasting blood sugar, resting ecg, maximum heart rate, exercise'
-#                           'induced angina and number of major vessels among others.'),
-#     ])
-#     return jsonify(data="Hello from wtf.")
-#
-# # @app.route("/predict", methods='POST')
-# # def dummy_api():
-# #     return jsonify(data="Hello from me.",
-# #                    tae="boto")
-#
-# app.add_url_rule('/predict', 'webio_view', webio_view(predict),
-#                  methods=['GET', 'POST', 'OPTIONS'])
-#
-# if __name__ == '__main__':
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument("-p", "--port", type=int, default=8080)
-#     args = parser.parse_args()
-#
-#     start_server(predict, port=args.port)
-#
-# # if __name__ == '__main__':
-# #     # parser = argparse.ArgumentParser()
-# #     # parser.add_argument("-p", "--port", type=int, default=8080)
-# #     # args = parser.parse_args()
-# #     #
-# #     # start_server(dummy_api, port=args.port)
-# #     app.run()
