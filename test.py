8974ca0a78d6d7a7c54b417b264ce5ac2c10c327
mcabanlit <102983286+mcabanlit@users.noreply.github.com>
2022-07-03 21:18:06 +0800

    Updated Requirements

--- test.py
+++ test.py
@@ -1,8 +1,20 @@
 import requests
-r = requests.post('http://127.0.0.1:5000/predict',
-                 json={
-                     "Store": 1111
-                 })
+# r = requests.post('http://127.0.0.1:5000/predict',
+#                  json={
+#                      "Store": 1111
+#                  })
 
+
+
+r = requests.post('https://drug-store-chain.herokuapp.com/predict', json={
+                      "Store":1111,
+                      "DayOfWeek":4,
+                      "Date":"2014-07-10",
+                      "Customers":410,
+                      "Open":1,
+                      "Promo":0,
+                      "StateHoliday":"0",
+                      "SchoolHoliday":1
+                 })
 
 print(r.text)
